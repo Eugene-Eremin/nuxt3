b819b4d6466565ce885cee3e15897075a1c5e24e
Eugene-Eremin <duser20102016@gmail.com>
2023-01-05 03:14:19 +0500

    second commit

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -6,8 +6,10 @@ export default defineEventHandler(async (event) => {
     // // handle post data
     // const { age } = await readBody(event)
 
+    const { currencyKey } = useRuntimeConfig()
+
     // api call with private key
-    const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=9Bwr5JTNtOTALaPcebmLEkpa1ohXk6obcW4XK1WP')
+    const { data } = await $fetch(`https://api.currencyapi.com/v3/latest?apikey=${currencyKey}`)
     
     return data
 })
